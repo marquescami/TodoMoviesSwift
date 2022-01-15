a69436b808bb4ed4fa880498ec9ba93084a02b8c
marquescami <camilamsc@hotmail.com>
2022-01-14 21:39:34 -0300

    criando tela estatica

---
+++ TodoMoviesSwift/View/StaticView.swift
@@ -0,0 +1,20 @@
+//
+//  StaticView.swift
+//  TodoMoviesSwift
+//
+//  Created by Camila Marques Vasconcelos Loureiro on 14/01/22.
+//
+
+import SwiftUI
+
+struct StaticView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StaticView_Previews: PreviewProvider {
+    static var previews: some View {
+        StaticView()
+    }
+}
